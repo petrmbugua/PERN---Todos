25e157c4d9c8c0f44a07f844f002a886c2c688ea
petrmbugua <petrmbugua@yahoo.com>
2021-02-10 15:06:34 +0300

    list todos

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Table, Jumbotron, Form, Button } from 'react-bootstrap';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = () => {
+    axios
+      .get('http://localhost:5000/todos')
+      .then((res) => {
+        const todos = res.data;
+        setTodos(todos);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Jumbotron>
+        <h1>Todos</h1>
+        <p>CRUD</p>
+      </Jumbotron>
+
+      <Form className='py-3'>
+        <Form.Group>
+          <Form.Label>Todo</Form.Label>
+          <Form.Control type='text' placeholder='Enter todo' />
+        </Form.Group>
+
+        <Button variant='primary' type='submit'>
+          Add
+        </Button>
+      </Form>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        {todos.map((item) => (
+          <tbody key={item.todo_id}>
+            <tr>
+              <td>{item.todo_id}</td>
+              <td>{item.description}</td>
+            </tr>
+          </tbody>
+        ))}
+      </Table>
+    </Container>
   );
-}
+};
 
 export default App;
